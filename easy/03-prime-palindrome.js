6911843c67de9284235e0b086b9a8bf3bd5902a3
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-01 20:31:54 +0000

    Updated solution

--- easy/03-prime-palindrome.js
+++ easy/03-prime-palindrome.js
@@ -1,23 +1,31 @@
 //https://www.codeeval.com/open_challenges/3/
-var output = 1, largestPrime = '';
 
-while(output < 1000) {
-    if(isPrime(output)) {
-        largestPrime = output;
-    }
-    output++;
-}
-console.log(largestPrime);
+var isPrime = function(num) {
+  var i = 2;
 
-function isPrime(n) {
-    var x;
-    for(x=2;x<n;x++) {
-        if(n % x === 0) {
-            return false;
-        }
+  for(i; i < num; i++) {
+    if(num % i === 0) {
+      return false;
     }
-    if(n.toString() === n.toString().split('').reverse().join('')) {
-        return true;
+  }
+  return true;
+};
+
+var isPalindrome = function(n) {
+  n = n.toString();
+  return (n) === (n).split('').reverse().join('');
+};
+
+var getLargestPrime = function(max, i) {
+  while(i < max) {
+    if(isPrime(i) && isPalindrome(i)) {
+      largestPrime = i;
     }
-    return false;
-}
+    i++;
+    return getLargestPrime(max, i);
+  }
+
+  return largestPrime;
+};
+
+console.log(getLargestPrime(1000, 1));
