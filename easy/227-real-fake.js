6ef354f3735e03eb57b92a8a468727d7ba9ee190
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-01 19:46:08 +0000

    Updates to solution

---
+++ easy/227-real-fake.js
@@ -0,0 +1,15 @@
+//https://www.codeeval.com/open_challenges/227/
+
+var fs  = require("fs");
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+  if(line !== '') {
+
+    var toArray = function(str) {
+      return str.split(' ');
+    };
+
+    var
+
+
+  }
+});
