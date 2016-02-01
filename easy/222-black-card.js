21adde7d066da5c34c469b43bc54796e653d0d29
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-01 19:33:45 +0000

    unfinished solution updates

--- easy/222-black-card.js
+++ easy/222-black-card.js
@@ -1,1 +1,42 @@
 //https://www.codeeval.com/open_challenges/222/
+
+var fs  = require("fs");
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+  if(line !== '') {
+
+    var sanitise = function(str) {
+        return str
+          .trim()
+          .replace(/\r/, '');
+    };
+
+    var getStrItem = function(str, index) {
+      str = str.split(' | ');
+      return str[index];
+    };
+
+    var toArray = function(str, separator) {
+        return str.split(separator || '');
+    };
+
+    var getPirate = function(players, pirate) {
+      var allPlayers = players,
+          maxIterations = pirate - 1,
+          lenAllPlayers = allPlayers.length;
+
+      while(lenAllPlayers > 1) {
+        allPlayers = players.filter(function(player, index) {
+          return index !== maxIterations;
+        });
+
+        lenAllPlayers = allPlayers.length;
+      }
+    };
+
+    var str = sanitise(line),
+        players = getStrItem(str, 0),
+        pirates = getStrItem(str, 1);
+
+    getPirate(toArray(players, ' '), pirates);
+  }
+});
