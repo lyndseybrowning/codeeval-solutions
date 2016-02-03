57683c779c5723dc71eab5e6daac2b23ce8c1bf7
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-03 19:19:16 +0000

    Updated 30-set-intersection

--- easy/30-set-intersection.js
+++ easy/30-set-intersection.js
@@ -1,18 +1,23 @@
-//https://www.codeeval.com/open_challenges/30/
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-   if (line !== "") {
-        line = line.split(';');
-        var	left = line[0].split(','),
-        	right = line[1],
-        	inter = '';
+// https://www.codeeval.com/open_challenges/30/
 
-        for(var i = 0, l=left.length; i<l; i++) {
-        	var num = left[i];
-        	if(right.indexOf(num) !== -1) {
-        		inter+=','+num;
-        	}
-        }
-        console.log(inter.substring(1));
-    }
+var fs  = require('fs');
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+  if (line !== '') {
+
+    var getArray = function(str, separator) {
+      return str.split(separator);
+    };
+
+    var getIntersection = function(left, right) {
+      var intersection = left.filter(function(item, index) {
+        return right.indexOf(item) !== -1;
+      });
+
+      return intersection.length ? intersection.join(',') : '';
+    };
+
+    var sections = getArray(line, ';');
+
+    console.log(getIntersection(getArray(sections[0], ','), getArray(sections[1], ',')));
+  }
 });
