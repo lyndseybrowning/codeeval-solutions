ec6a1bbd39dbec5f08806da53a4c74b24f898b87
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-08 19:35:23 +0000

    Updated armstrong numbers

--- easy/82-armstrong-numbers.js
+++ easy/82-armstrong-numbers.js
@@ -1,19 +1,29 @@
-//https://www.codeeval.com/open_challenges/82/
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    if (line !== "") {
-        console.log(isArmstrong(line));
+// https://www.codeeval.com/open_challenges/82/
+
+var fs  = require('fs');
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+    if (line !== '') {
+
+      var getDigit = function(n) {
+        return parseInt(n, 10);
+      };
+
+      var getDigitsArr = function(str) {
+        return str.split('').map(function(item) {
+          return getDigit(item);
+        });
+      };
+
+      var sumOfDigits = function(digits) {
+        return digits.reduce(function(prev, current) {
+          return prev + Math.pow(current, digits.length);
+        }, 0);
+      };
+
+      var isArmstrongNumber = function(digits, number) {
+        return sumOfDigits(digits) === number;
+      };
+
+      console.log(isArmstrongNumber(getDigitsArr(line), getDigit(line)) ? 'True' : 'False');
     }
 });
-
-function isArmstrong(n) {
-    var len = n.length,
-        sum = 0, num, pow;
-
-    for(var i=0;i<len;i++) {
-        num = parseInt(n[i]);
-        pow = Math.pow(num,len);
-        sum+=pow;
-    }
-    return (sum === parseInt(n) ? 'True' : 'False');
-}
