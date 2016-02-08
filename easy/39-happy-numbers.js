8f0c84978c1a58330f0dd61ca2a7b905884b7fe4
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-08 18:49:34 +0000

    Updated happy numbers

--- easy/39-happy-numbers.js
+++ easy/39-happy-numbers.js
@@ -1,29 +1,38 @@
-//https://www.codeeval.com/open_challenges/39/
-var t, values = [];
+// https://www.codeeval.com/open_challenges/39/
 
-function getHappy(n) {
-    t = 0;
+var fs  = require('fs');
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+  if (line !== '') {
 
-    for(var i=0, len = n.length; i<len;i++) {
-        var item = n[i];
-        t+=Math.pow(item,2);
-    }
+    var getDigits = function(str) {
+      return str.split('').map(function(n) {
+        return parseInt(n, 10);
+      });
+    };
 
-    if(t === 1) {
-        console.log('1');
-    } else if(values.indexOf(t) !== -1) {
-        console.log('0');
-    } else {
-        values.push(t);
-        getHappy(t.toString());
-    }
-}
+    var sumOfDigits = function(digits) {
+      return digits.reduce(function(prev, current) {
+        return prev + (current * current);
+      }, 0);
+    };
 
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    if (line !== "") {
-        values = [];
-        getHappy(line);
-    }
+    var sumOfSquares = function(str, exSums) {
+      exSums = exSums || [];
 
+      var sum = sumOfDigits(getDigits(str));
+
+      if(sum === 1) {
+        return 1;
+      }
+
+      if(exSums.indexOf(sum) !== -1) {
+        return 0;
+      } else {
+        exSums.push(sum);
+      }
+      return sumOfSquares(sum.toString(), exSums);
+    };
+
+    console.log(sumOfSquares(line));
+  }
 });
