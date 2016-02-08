5c80fc234af9a6e105c2b12a6b47517db99ec966
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-08 19:06:07 +0000

    Updated self describing numbers

--- easy/40-self-describing-numbers.js
+++ easy/40-self-describing-numbers.js
@@ -1,18 +1,27 @@
-//https://www.codeeval.com/open_challenges/40/
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    if (line !== "") {
-        var o = 1, item, pattern, match;
-        for(var i=0, len = line.length; i<len; i++) {
-            item = parseInt(line[i]);
-            pattern = new RegExp( i, 'g' );
-            match = (line.match(pattern) || []).length;
+// https://www.codeeval.com/open_challenges/40/
 
-            if (item !== match) {
-                o=0;
-                break;
-            }
-        }
-        console.log(o);
-    }
+var fs  = require('fs');
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+  if (line !== '') {
+
+    var getDigits = function(str) {
+      return str.split('').map(function(n) {
+        return parseInt(n, 10);
+      });
+    };
+
+    var getInstancesOf = function(arr, index) {
+      return arr.filter(function(item) {
+        return item === index;
+      }).length;
+    };
+
+    var selfDescribing = function(digits) {
+      return digits.every(function(digit, index) {
+        return getInstancesOf(digits, index) === digit;
+      });
+    };
+
+    console.log(selfDescribing(getDigits(line)) ? 1 : 0);
+  }
 });
