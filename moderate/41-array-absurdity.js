d3c553916ea951db34194873708637a2d8a8f6b6
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-15 19:13:14 +0000

    Updated array absurdity

--- moderate/41-array-absurdity.js
+++ moderate/41-array-absurdity.js
@@ -1,21 +1,35 @@
-//https://www.codeeval.com/open_challenges/41/
+// https://www.codeeval.com/open_challenges/41/
 
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-   line = line.trim();
-	if (line !== "") {
-        var arr = [],
-            numbers = line.split(";")[1].split(","),
-            duplicate;
+var fs  = require('fs');
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+	if (line !== '') {
+    var getArray = function(str, separator, index) {
+      var arr = str.split(separator);
 
-        for(var i = 0, l = numbers.length; i<l; i++) {
-            var num = numbers[i];
-            if(-1 !== arr.indexOf(num)) {
-            	duplicate = num;
-            	break;
-            }
-            arr.push(num);
+      if(typeof index !== 'undefined') {
+        return arr[index];
+      }
+      return arr;
+    };
+
+    var findDuplicate = function(arr) {
+      var digits = getArray(arr, ','),
+          duplicated = [],
+          duplicatedDigit;
+
+      digits.some(function(digit, index) {
+
+        if(duplicated.indexOf(digit) === -1) {
+          duplicated.push(digit);
+        } else {
+          duplicatedDigit = digit;
+          return;
         }
-        console.log(duplicate);
-    }
+      });
+
+      return duplicatedDigit;
+    };
+
+    console.log(findDuplicate(getArray(line, ';', 1)));
+  }
 });
