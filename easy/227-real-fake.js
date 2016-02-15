3d90f2fa448a6e3b767deea2d88670fc13023580
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-15 18:27:26 +0000

    Finished solution

--- easy/227-real-fake.js
+++ easy/227-real-fake.js
@@ -1,15 +1,31 @@
-//https://www.codeeval.com/open_challenges/227/
+// https://www.codeeval.com/open_challenges/227/
 
 var fs  = require("fs");
 fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
   if(line !== '') {
 
     var toArray = function(str) {
-      return str.split(' ');
+      return str.split('');
     };
 
-    var
+    var toDigits = function(arr) {
+      return arr.map(function(item) {
+        return parseInt(item, 10);
+      });
+    };
 
+    var realOrFake = function(digits) {
+      var sum = digits.filter(Boolean)
+        .reduce(function(prev, current, index) {
+          if(index % 2 === 0) {
+            return prev + (current * 2);
+          }
+          return prev + current;
+        }, 0);
 
+        return (sum % 10 === 0) ? 'Real' : 'Fake';
+    };
+
+    console.log(realOrFake(toDigits(toArray(line))));
   }
 });
