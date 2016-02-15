9393b6e30b1a81d1b5e9eea4fe9768ddbe2d25ee
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-15 19:24:25 +0000

    Updated counting primes

--- moderate/63-counting-primes.js
+++ moderate/63-counting-primes.js
@@ -1,28 +1,38 @@
-//https://www.codeeval.com/open_challenges/63/
+// https://www.codeeval.com/open_challenges/63/
 
-var fs  = require("fs");
-fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-   if (line !== "") {
-        var numPrimes = 0,
-            spl = line.split(","),
-            n = parseInt(spl[0]),
-            m = parseInt(spl[1]);
+var fs  = require('fs');
+fs.readFileSync('test.txt').toString().split('\n').forEach(function (line) {
+   if (line !== '') {
 
-        while(n <= m) {
-            if(isPrime(n)) {
-                numPrimes+=1;
-            }
-            n++;
-        }
-        console.log(numPrimes);
+     var getArray = function(str, index) {
+       return str.split(',').map(function(item) {
+         return parseInt(item, 10);
+       })[index];
+     };
+
+     var isPrime = function(n) {
+       var i = 2;
+       while(i < n) {
+         if(n % i === 0) {
+             return false;
+         }
+         i++;
+       }
+       return true;
+     };
+
+     var countPrimes = function(min, max) {
+       var numPrimes = 0;
+
+       while(min <= max) {
+         if(isPrime(min)) {
+           numPrimes++;
+         }
+         min++;
+       }
+       return numPrimes;
+     };
+
+     console.log(countPrimes(getArray(line, 0), getArray(line, 1)));
     }
 });
-
-function isPrime(num) {
-    for(var i = 2; i<num; i++) {
-        if(num % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
